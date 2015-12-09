21a837ea1517077af276850ed678d1c354a5f032
SA <samuel.amo@gmail.com>
2015-12-09 18:37:06 +0100

    Updating.

--- functions.c
+++ functions.c
@@ -1,6 +1,16 @@
+#define USER_CONFIG_LOCATION ".config/sysgeep/"
+#define ROOT_CONFIG_LOCATION "/etc/sysgeep/"
+#define CONFIG_FILE_NAME "sysgeep.conf"
+
 int sysgeep_setup(char * local_git_repo_path)
 {
    int ret = 0;
+
+   //
+   //if no option -r, check whether the user is root
+   //if root, use /etc/sysgeep/sysgeep.conf
+   //if not root, use 
+
    return ret;
 }
 
