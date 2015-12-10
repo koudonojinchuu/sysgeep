9142818b28339aeaf7ef5d572f1d3b32e59e202f
SA <samuel.amo@gmail.com>
2015-12-10 18:02:03 +0100

    wip create missing dirs

--- functions.c
+++ functions.c
@@ -1,17 +1,22 @@
 #include <stdlib.h> // getenv(), abort()
 #include <stdio.h>  // fprintf()
 #include <string.h> // strlen()
+#include <sys/stat.h>
 
-#define USER_CONFIG_LOCATION ".config/sysgeep/sysgeep.conf"
+#define USER_CONFIG_FILE "sysgeep.conf"
+#define USER_CONFIG_DIR ".config/sysgeep/"
+#define USER_CONFIG_LOCATION USER_CONFIG_DIR##USER_CONFIG_FILE
 #define ROOT_CONFIG_LOCATION "/etc/sysgeep/sysgeep.conf"
 
 //if option -s, use /etc/sysgeep.conf
 //else, use $HOME/.conf/sysgeep/sysgeep.conf
 char * config_file(int sflag)
 {
+   char * config_file_path;
+
    if (sflag)
    {
-      return ROOT_CONFIG_LOCATION;
+      config_file_path = ROOT_CONFIG_LOCATION;
    }
    else
    {
@@ -19,12 +24,31 @@ char * config_file(int sflag)
       if (homedir == NULL)
       {
          fprintf(stderr, "Error: HOME variable is not set.\n");
-         fprintf(stderr, "try -s option to use system wide /etc/sysgeep/sysgeep.conf\n");
+         fprintf(stderr, "try -s option to use system wide "ROOT_CONFIG_LOCATION"\n");
          abort();
       }
-      char * config_file_path = malloc(sizeof(char)*(strlen(USER_CONFIG_LOCATION) + strlen(homedir) + 1));
+      config_file_path = malloc(sizeof(char)*(strlen(USER_CONFIG_LOCATION) + strlen(homedir) + 1));
       sprintf(config_file_path, "%s/"USER_CONFIG_LOCATION, homedir);
-      return config_file_path;
+
+      // create directory if missing
+      char * directory_to_check = malloc(sizeof(char)*(strlen("/.config") + strlen(homedir) + 1));
+      sprintf(directory_to_check, "%s/.config", homedir);
+      struct stat s;
+      if (!(stat(directory_to_check, s) == 0 && S_ISDIR(s.st_mode)))
+      {
+        mode_t old_mask = umask(0);
+        int md_ret = mkdir(USER_CONFIG_DIR, "700");
+        unmask(old_mask);
+      }
+   }
+}
+
+void create_config_dir(int sflag)
+{
+   if (!sflag) // case of $HOME/.conf/sysgeep/sysgeep.conf
+   {
+      struct stat s;
+      stat("", s)
    }
 }
 
@@ -32,6 +56,9 @@ int sysgeep_setup(char * local_git_repo_path, int sflag)
 {
    int ret = 0;
    char * config_file = config_file(sflag);
+   create_config_dir(sflag);
+
+   //
 
    return ret;
 }
