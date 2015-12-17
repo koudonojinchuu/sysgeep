1de22c60997fce2ca111f64e9717c8a9e61984e4
SA <samuel.amo@gmail.com>
2015-12-17 17:17:21 +0100

    next bug

--- src/sorted_lines.c
+++ src/sorted_lines.c
@@ -6,6 +6,29 @@
 
 #include "utils.h" // chk(), chk_t(), pchk_t()
 
+// the difference VS a raw char ** is that length can be 0
+typedef struct struct_lines_array {
+  char ** array;
+  int length;
+} s_lines_array;
+
+static s_lines_array * create_lines_array(int length)
+{
+  s_lines_array * array_obj = malloc(sizeof(s_lines_array));
+  array_obj->length = length;
+  array_obj->array = malloc(sizeof(char *)*length);
+  return array_obj;
+}
+
+static void free_lines_array(s_lines_array * lines_array)
+{
+  int i;
+  int len = lines_array->length;
+  for (i=0; i<len; i++) free(lines_array->array[i]);
+  free(lines_array->array);
+  free(lines_array);
+}
+
 void init_counted_file(char * counted_file_path)
 {
   // do nothing if the counted file already exists
@@ -26,7 +49,7 @@ void init_counted_file(char * counted_file_path)
 }
 
 // the first line must be the lines count (that's a "counted file")
-static char ** counted_file_to_lines_array(char * file_path)
+static s_lines_array * counted_file_to_lines_array(char * file_path)
 {
   FILE * fd = fopen(file_path, "r");
   pchk_t( fd, "Error, could not load lines from %s\n", file_path );
@@ -36,10 +59,10 @@ static char ** counted_file_to_lines_array(char * file_path)
   size_t n = 0;
   getline(&line, &n, fd);
   int nb_lines = atoi(line);
+  printf("recorded lines nb: %d\n", nb_lines);
 
   // allocate array of line pointers
-  char ** lines;
-  lines = malloc(sizeof(char *)*nb_lines);
+  s_lines_array * lines = create_lines_array(nb_lines);
 
   // load all other lines in an array
   free(line);
@@ -47,7 +70,9 @@ static char ** counted_file_to_lines_array(char * file_path)
   int line_count = 0;
   while (getline(&line, &n, fd) != -1)
   {
-    lines[line_count] = line;
+    int line_end = strlen(line) - 1;
+    if ((line_end >= 0) && (line[line_end] == '\n')) line[line_end]='\0';
+    lines->array[line_count] = line;
     line = NULL;
     line_count++;
   }
@@ -57,22 +82,14 @@ static char ** counted_file_to_lines_array(char * file_path)
 }
 
 // to write down a lines array into a "counted file"
-static void lines_array_to_counted_file(char ** lines, char * counted_file_path)
+static void lines_array_to_counted_file(s_lines_array * lines, char * counted_file_path)
 {
-  int len = sizeof(lines) / sizeof(char *);
+  int len = lines->length;
   FILE * fd = fopen(counted_file_path, "w+");
   fprintf(fd, "%d\n", len);
   int i;
-  for (i=0; i<len; i++) fprintf(fd, "%s\n", lines[i]);
+  for (i=0; i<len; i++) fprintf(fd, "%s\n", lines->array[i]);
   fclose(fd);
-}
-
-static void free_lines_array(char ** lines_array)
-{
-  int i;
-  int len = sizeof(lines_array) / sizeof(char *);
-  for (i=0; i<len; i++) free(lines_array[i]);
-  free(lines_array);
 }
 
 // return the length of the line truncated to the first word (the key, i.e. the pathname)
@@ -116,7 +133,7 @@ static int comparison_function(char * line1, char * line2)
   return 0;
 }
 
-static int helper__index_of_closest_predecessor(char ** lines_array, char * str_to_lookup, int * found, int beg, int end)
+static int helper__index_of_closest_predecessor(s_lines_array * lines_array, char * str_to_lookup, int * found, int beg, int end)
 {
   if (beg > end) return -1;
   int mid = (end + beg) / 2;
@@ -124,7 +141,7 @@ static int helper__index_of_closest_predecessor(char ** lines_array, char * str_
   // if str_to_lookup == lines_array[mid], return mid
   // if str_to_lookup > lines_array[mid], return helper__(mid + 1, end)
   // if str_to_lookup < lines_array[mid], return helper__(beg, mid - 1)
-  int comp = comparison_function(str_to_lookup, lines_array[mid]);
+  int comp = comparison_function(str_to_lookup, lines_array->array[mid]);
   if (comp == 0)
   {
     *found = 1;
@@ -146,9 +163,9 @@ static int helper__index_of_closest_predecessor(char ** lines_array, char * str_
 // If some line has exactly that name, will return its index.
 // Else, return the line that comes just before it (its lower bound).
 // If there are no lines in the array, return -1
-static int index_of_closest_predecessor(char ** lines_array, char * str_to_lookup, int * found)
+static int index_of_closest_predecessor(s_lines_array * lines_array, char * str_to_lookup, int * found)
 {
-  int len = sizeof(lines_array) / sizeof(char *);
+  int len = lines_array->length;
   if (!len) return -1;
   return helper__index_of_closest_predecessor(lines_array, str_to_lookup, found, 0, len - 1);
 }
@@ -157,20 +174,29 @@ static int index_of_closest_predecessor(char ** lines_array, char * str_to_looku
 void add_sorted_line(char * sysgeep_index_path, char * attributes_buffer)
 {
   // load all lines into an array
-  char ** lines = counted_file_to_lines_array(sysgeep_index_path);
-  int len = sizeof(lines) / sizeof(char *);
+  s_lines_array * lines = counted_file_to_lines_array(sysgeep_index_path);
+  int len = lines->length;
 
   // search for the index where to insert
   int found = 0;
   int pred_index = index_of_closest_predecessor(lines, attributes_buffer, &found);
 
   // insert (or replace if found==1)
-  char ** new_array = malloc(sizeof(char *)*(len + 1));
+  s_lines_array * new_array = create_lines_array(len + !found);
   int i;
-  for (i=0; i<=pred_index; i++) new_array[i] = lines[i];
-  new_array[1+pred_index] = attributes_buffer;
-  for (i=pred_index+2+found; i<len+1; i++) new_array[i] = lines[i-1];
-  // the array "lines" gave its elements to "new_array", so simple free()
+  for (i=0; i<=pred_index; i++) new_array->array[i] = lines->array[i];
+  new_array->array[1+pred_index] = attributes_buffer;
+  for (i=pred_index+2+found; i<len+1; i++) new_array->array[i] = lines->array[i-1];
+
+  printf("@@@@\n");
+  printf("len: %d\n", lines->length);
+  for (int j=0; j<lines->length; j++) printf("%s\n", lines->array[j]);
+  printf("@@@@\n");
+  printf("len: %d\n", new_array->length);
+  for (int j=0; j<new_array->length; j++) printf("%s\n", new_array->array[j]);
+  printf("@@@@\n");
+  // the array "lines" gave its elements to "new_array" so free only the remaining
+  free(lines->array);
   free(lines);
 
   // write back to a new file on disk
