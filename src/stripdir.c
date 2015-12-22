0733b6dd4286ab30d9fb3500e8713926d8fb02da
SA <samuel.amo@gmail.com>
2015-12-22 12:45:09 +0100

    ready to test stripdir

--- src/stripdir.c
+++ src/stripdir.c
@@ -1,72 +1,92 @@
-// the same behaviour as the commad "realpath -s"
+// the same behaviour as the command "realpath -s"
 char *realpath_s(char * pathname) {
-	int ldots;
-	ldots = 0;
-
-	//in   = dir;
-	//out  = buf;
-	//last = buf + maxlen;
-	//*out  = 0;
 
 	char * result;
+  char * cwd = NULL;
+  int cwd_len = 0;
+  int cwd_needs_trailing_slash = 0;
   // if pathname is relative, use getcwd()
 	if (pathname[0] != '/') {
     // error if cwd cannot be known
-    char * cwd;
 		if ( !(cwd = getcwd(NULL, 0)) ) return NULL;
-
-    // make sure that the last char of cwd is '/'
-    int cwd_len = strlen(cwd);
-    //out = buf + strlen(buf) - 1;
-    if (cwd[cwd_len - 1] != '/')
-    {
-      char * cwd = realloc(cwd, sizeof(char*)*(2+cwd_len));
-      sprintf(cwd, "%s/", cwd);
-    }
-    //out++;
+    // make sure that cwd will be terminated by '/'
+    cwd_len = strlen(cwd);
+    if (cwd[cwd_len - 1] != '/') cwd_needs_trailing_slash = 1;
 	}
 
-	while (out < last) {
-		*out = *in;
+  // the maximum required size for the buffer will be 
+  // len_cwd + len_pathname + 2
+  int pathname_len = strlen(pathname);
+  int buf_len = cwd_len + pathname_len + 1;
+  char * buf_ptr;
+  char * buf = malloc(sizeof(char)*(buf_len + 1));
+  char * buf_last = buf + buf_len; // pointer to the last usable character of buf (included terminating '\0')
+  // after that, buf_ptr points to the character just after cwd/
+  if (cwd_needs_trailing_slash)
+  {
+    sprintf(buf, "%s/", cwd);
+    buf_ptr = buf + cwd_len + 1;
+  }
+  else if (cwd)
+  {
+    sprintf(buf, "%s", cwd);
+    buf_ptr = buf + cwd_len;
+  }
+  else
+  {
+    buf[0] = '\0';
+    buf_ptr = buf;
+  }
+
+  char * pathname_ptr = pathname;
+	int dots = 0;
+	while (buf_ptr < buf_last) {
+		*buf_ptr = *pathname_ptr; // copy chars from pathname to current buf_ptr
 
     // if we are on '/', go ahead, then if again on a '/', go ahead,
     // until the current char is not '/', then go to immediately  previous '/'
-		if (*in == '/')
+		if (*pathname_ptr == '/')
 		{
-			while (*(++in) == '/') ;
-			in--;
+			while (*(++pathname_ptr) == '/') ;
+			pathname_ptr--;
 		}
 
-    // if in[0]=='\0' or if on a '/'
-		if (*in == '/' || !*in)
+    // if pathname_ptr[0]=='\0' or if on a '/'
+		if (*pathname_ptr == '/' || !*pathname_ptr)
 		{
-      // go backwards and reduce .. or . occurrences
-			if (ldots == 1 || ldots == 2) {
-				while (ldots > 0 && --out > cwd) if (*out == '/') ldots--;
-        out[1]='\0'; // terminates temporarily the result string
+      // we reached a slash or the end, so go backwards and reduce the last recorded .. or . occurrence
+			if (dots == 1 || dots == 2) {
+				while (dots > 0 && --buf_ptr > cwd) if (*buf_ptr == '/') dots--;
+        buf_ptr[1]='\0'; // terminates temporarily the result string
 			}
-			ldots = 0;
+      // cancel any 'event' occurence, because we met a slash (or are at the begining of the string)
+			dots = 0;
 
-		} else if (*in == '.' && ldots > -1) {
-			ldots++;
-		} else {
-			ldots = -1;
+    // if 'event' did not occur, and we are on a dot, count it
+		} else if (*pathname_ptr == '.' && dots > -1) {
+			dots++;
+		} else { // 'event' occurs: we reached a non-dot, non-slash character
+			dots = -1;
 		}
 
-		out++;
+    // increasing 'buf_ptr' pointer
+		buf_ptr++;
 
-		if (!*in)
-			break;
+    // if we reached the end of pathname (ie '\0' char), exit while loop
+		if (!*pathname_ptr) break;
 
-		in++;
+    // increasing pathname pointer
+		pathname_ptr++;
 	}
 
-	if (*in) {
+  // error if we reached "buf_last" but some characters of pathname are still left
+	if (*pathname_ptr) {
 		errno = ENOMEM;
 		return NULL;
 	}
 
-	while (--out != buf && (*out == '/' || !*out)) *out=0;
+  // replace all trailing slashes by null chars
+	while (--buf_ptr != buf && (*buf_ptr == '/' || !*buf_ptr)) *buf_ptr=0;
 	return buf;
 }
 
