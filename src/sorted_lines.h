811432964fdfdea3b5cf0f28dcb65d48e02ea979
SA <samuel.amo@gmail.com>
2015-12-22 18:03:31 +0100

    wip

--- src/sorted_lines.h
+++ src/sorted_lines.h
@@ -1,3 +1,3 @@
 void init_counted_file(char * counted_file_path);
-void add_sorted_line(char * sysgeep_index_path, char * attributes_buffer);
-void lookup_sorted_line(char * sysgeep_index_path, char * file_path);
+void add_sorted_line(char * index_path, char * attributes_buffer);
+char * lookup_sorted_line(char * index_path, char * file_path);
