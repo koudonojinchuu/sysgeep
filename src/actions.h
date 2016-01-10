edf64fc8ecb4798b65aeb0a104abea570a4636b3
Samuel Amo <samuel.amo@gmail.com>
2016-01-11 00:14:01 +0100

    start work on remote

--- src/actions.h
+++ src/actions.h
@@ -2,3 +2,6 @@ int sysgeep_print_setup(int sflag);
 int sysgeep_setup(char * local_git_repo_path, int sflag);
 int sysgeep_save(char * file_path, int sflag);
 int sysgeep_restore(char * file_path, int sflag);
+
+int sysgeep_push(int sflag);
+int sysgeep_pull(int sflag);
